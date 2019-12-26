a1ac3b3e65ca17d5beec5fa9e9e5fd985a4078f6
inarreol <56372279+inarreol@users.noreply.github.com>
2019-12-25 21:26:26 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,93 @@
+var config = {
+    style: 'mapbox://styles/inarreol/ck4hm61bs2zk21doey8mxfbmr',
+    accessToken: 'pk.eyJ1IjoiaW5hcnJlb2wiLCJhIjoiY2p1ZnB2MWh5MGd1NDQ0cGozN29kYmR3NSJ9.14s63N3V8r2xcufGCZrnqw',
+    showMarkers: false,
+    theme: 'dark',
+    alignment: 'left',
+    title: 'Mole: a journey on its history',
+    subtitle: '',
+    byline: 'By Ingrid Arreola',
+    footer: 'Source: Instituo de Turismo, Mexico | Image from Wikipedia',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            title: 'Mole Deconstructed',
+            image: 'mole.jpeg',
+            description: 'Mole, a thick, rich sauce, often served with meats and rice is available in many form and varieties. Its history is rich and its name comes from the Nahuatl word for sauce – mōlli. Mole has early references in writings as a dish served to the Aztec Moctezuma. Long before the Spanish arrived to Mexico, indigenous women were grinding chile, tomatoes, corn, and other ingredients found in mole.',
+            //location: {
+                center: [-10.10266, 37.98621],
+                zoom: 4.15,
+                pitch: 0.00,
+                bearing: 0.00//
+            },
+            //onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1
+                // }
+            //],
+            //onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]  
+        },
+        {
+            id: 'identifier-2',
+            title: 'Non-Indigenous Ingredients',
+            image: './path/to/image/source.png',
+            description: 'Mole also has non-indigenous ingredients such as cinnamon, clove, cumin, and sesame seeds. Mole may have deep roots in the Middle East. Large parts of Spain, almost the entire Iberian Peninsula was once a part of a Caliphate ruled by North African Muslims, the Moors. Traditions of this region were brought to Spain, including their dishes. Cookbooks of this region have sauces that are made with ground spices, nuts, and acid elements. While these sauces lack the chile and tomato (Mexican Indigenous ingredients) the sauce backbone is present. Ground nuts, seeds, spices, acidic elements, and liquid is central to Medieval Islamic cuisine and can be traced back to the golden age of Islam. Over time the Spanish began to adopt these ingredients to their own cuisines.',
+            location: {
+                center: [-10.10266, 37.98621],
+                zoom: 4.15,
+                pitch: 0.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'identifier-3',
+            title: 'Early References of Mole',
+            image: './path/to/image/source.png',
+            description: 'Mole has early references in writings as a dish served to the Aztec Moctezuma. The language of the Aztecs, Nahuatl, references molli as a sauce served with meats. Books mentioning this dish include Avila Blancas, San Pelayo, Gesta Independencia, and Mexicano Anonimo. These books mention several variations of mole and take place in 1690 – 1817. The ingredients in these books show non-indigenous materials introduced by the Spanish. Mole has changed and there is also history with its production in convents.  Convents in colonial Mexico were powerful because they owned land, had money, and resources. Nuns had the ability to have a career and own property. Nuns were able to get ingredients from their land, run a commercial kitchen with their indigenous slaves/servants, and oversaw the production of mole. On one side it was a business, however it was also the preparation for an elegant dish. The convents may have been the place to bring together the ingredients and traditions to create the staple dish. ',
+            location: {
+                center: [-100.28642, 21.02219],
+                zoom: 2.95,
+                pitch: 0.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'identifier-4',
+            title: 'Modern Day Mole',
+            image: './path/to/image/source.png',
+            description: 'Around the time of independence, cuisine was a way for people to distinguish themselves from the Spanish. This process involved focusing on the mestizaje, a mixing of indigenous and Spanish roots. Mole became of a symbol of this mixing.  Mole is a labor of love, and it is time consuming to mix together all the ingredients, and create the sauce. The metate, a grindstone, where you move a stone across a lower stone was commonly used. It demands a women kneel and prepare this complex sauce. The hand ground sauce was still ubiquitous across Mexico because of the indigenous labor available. With the introduction of electricity, there was a move from the metate to the blender. This move has allowed for this to be commercialized, sold, and available to all cooks.',
+            location: {
+                center: [-108.73662, 23.36550],
+                zoom: 4.85,
+                pitch: 0.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+{
+            id: 'identifier-5',
+            title: 'Mole in Oaxaca',
+            image: './path/to/image/source.png',
+            description: 'Oaxaca has many variations of mole, however there are 7 particular ones that represent the various regions of this state. Mole negro (black mole) has over 30 ingredients, is challenging to prepare, and has a more complex taste. Mole amarillo (yellow mole) is versatile and this version goes with several types of meats and vegetarian dishes as well. Coloradito (red color) is another type of mole, it’s name indicates that it is red. Mole verde (green mole) has ingredients that must be prepared with fresh ingredients and can be served with fish. Chichilo mole has a combination of fruits, meats, and leaves that give it a special taste. Mole rojo (red mole) and manchamanteles are the final two moles. While there are many more variations, these seven stands as the representatives for the state of Oaxaca.',
+            location: {
+                center: [-97.46529, 16.90960],
+                zoom: 7.57,
+                pitch: 0.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
